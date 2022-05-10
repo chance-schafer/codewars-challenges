e3b6d0644d484bc0a48c219ad3d7f6988c6408e6
chance <chance.schafer.dev@gmail.com>
2022-05-10 16:38:05 -0500

    solution016 completed

---
+++ js/solution016.js
@@ -0,0 +1,10 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ) {
+  if (bool === true) {
+    return 'Yes'
+  } else if (bool === false) {
+    return 'No'
+  }
+}
