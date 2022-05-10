80759c9f71d3e2fb8bf305caf7d2b3b7b1908fb5
chance <chance.schafer.dev@gmail.com>
2022-05-10 16:43:22 -0500

    solution022 completed

---
+++ js/solution022.js
@@ -0,0 +1,7 @@
+// List Filtering
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(arr) {
+  let newArray = arr.filter(val => typeof val === 'number');
+  return newArray
+}
