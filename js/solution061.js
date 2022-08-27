2b8c5b773f207f54d8c150db882f82e84a53e650
Chance Schafer <chance.schafer.dev@gmail.com>
2022-08-26 19:20:20 -0500

    solution061 completed

---
+++ js/solution061.js
@@ -0,0 +1,9 @@
+//Create Phone #
+
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(arr) {
+  return `(${arr.slice(0, 3).join("")}) ${arr.slice(3, 6).join("")}-${arr
+    .slice(6)
+    .join("")}`;
+}
